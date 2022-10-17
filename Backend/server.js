a659294c461af773342f2f151b37dc53d6449724
Gusben95 <bengtssongustav95@gmail.com>
2022-10-17 11:22:37 +0200

    Env / some server stuff

--- Backend/server.js
+++ Backend/server.js
@@ -1,26 +1,32 @@
 const express = require("express");
+// const https = require('https');
 // const helmet = require("helmet");
 // const rateLimit = require('express-rate-limit');
-const https = require('https');
 
-const PORT = 3001;
-const hostname = '127.0.0.1';
 const app = express();
+require('dotenv').config();
 
+const PORT = process.env.PORT;
+const DB_HOST = process.env.DB_HOST;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_DATABASE = process.env.DB_DATABASE;
+const DB_PORT = process.env.DB_PORT;
 
 // app.use(helmet());
 app.use(express.json())
 
-
-const server = https.createServer((req, res) => {
+const server = ((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
   });
   
-  server.listen(PORT, hostname, () => {
-    console.log(`Server running at http://${hostname}:${PORT}/`);
+  app.listen(PORT,  () => {
+    console.log(`Server running at 127.0.0.1:${PORT}/`);
   });
 
 
-
+  app.get('/', (req, res) => {
+    res.send("Try /getAllUsers or /createUser");
+  });
 
