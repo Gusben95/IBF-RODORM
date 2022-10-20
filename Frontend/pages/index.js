6ca507a0bcd4b12b1258026c713d67c98a360dbd
Philip Rydqvist <90709110+PhilipRydqvist@users.noreply.github.com>
2022-10-20 15:52:21 +0200

    login

--- Frontend/pages/index.js
+++ Frontend/pages/index.js
@@ -1,12 +1,28 @@
 import styles from '../styles/Home.module.css'
 import { useRouter } from 'next/router';
 import { useState } from 'react';
+import { useRef } from 'react';
 import Navbar from '../components/Navbar/Navbar';
 import Footer from '../components/Footer/Footer';
+import Axios from 'axios';
 
 export default function Home() {
   const usernameRef = useRef() 
   const passwordRef = useRef()
+  
+  /* const registerUser = () => {
+    Axios.post('http://localhost4000/createUser', {
+      username: usernameRef,
+      password: passwordRef,
+      body:JSON.stringify({
+        USERNAME: usernameRef.current.value,
+        PASSWORD: passwordRef.current.value,
+    }).then((response) => {
+      console.log(response);
+    })
+  })
+  } */
+  
   const registerUser = () =>  {
     
     fetch("http://127.0.0.1:4000/createUser", {
@@ -17,13 +33,30 @@ export default function Home() {
           // 'Content-Type': 'application/x-www-form-urlencoded',
         },
         body:JSON.stringify({
-            USERNAME: usernameRef.current.value,
-            PASSWORD: passwordRef.current.value  
+            username: usernameRef.current.value,
+            password: passwordRef.current.value  
         })
     })
-    // console.log(usernameRef.current.value)
-    // console.log(passwordRef.current.value)
+    /* // console.log(usernameRef.current.value)
+    // console.log(passwordRef.current.value) */
   }
+
+  const loginUser = () =>  {
+    
+    fetch("http://127.0.0.1:4000/loginUser", {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+          
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body:JSON.stringify({
+            username: usernameRef.current.value,
+            password: passwordRef.current.value  
+        })
+    })
+  }
+
   return (
     <div>
     <Navbar />
@@ -39,6 +72,8 @@ export default function Home() {
         
         </div>
         <button onClick={registerUser}>Registrera</button>
+        
+        <button onClick={ loginUser }>Login</button>
       </main>
 
      <Footer />
