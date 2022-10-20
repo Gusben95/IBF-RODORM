b563befb0e1f50c6657fc35afa2940b46898d9d3
Philip Rydqvist <90709110+PhilipRydqvist@users.noreply.github.com>
2022-10-20 11:41:52 +0200

    useref

--- Frontend/pages/index.js
+++ Frontend/pages/index.js
@@ -1,6 +1,7 @@
 import styles from '../styles/Home.module.css'
 import { useRouter } from 'next/router';
 import { useState } from 'react';
+import { useRef } from 'react';
 import Navbar from '../components/Navbar/Navbar';
 import Footer from '../components/Footer/Footer';
 
