a892e174678c4480db94f17be8eaa1b0789dc2f2
Philip Rydqvist <philip.rydqvist@gmail.com>
2022-11-02 04:31:31 +0100

    spelschema updatering

--- Frontend/pages/spelschema.js
+++ Frontend/pages/spelschema.js
@@ -7,8 +7,18 @@ const spelschema = () => {
             <Navbar />
             Information
         <div>
+            <p>Spelschema och serie:</p>
             
-            <a href="https://korpengoteborg.zoezi.se/member#/teamsports/division/96">Tryck för att se div 1 Korpen Göteborg</a>
+            <a href="https://korpengoteborg.zoezi.se/member#/teamsports/division/96"
+            >Länk till div 1 22/23 Korpen Göteborg
+            </a>
+            </div>
+            
+            <div>
+            <a href=
+            "https://idrottonline.se/KorpforeningGoteborg-Korpen/Idrotter/Innebandy/ResultatSpelprogram"
+            >Länk till seriehistorik
+            </a>
         </div>
             <Footer />
         </div>
