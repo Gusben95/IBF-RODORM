b88c31c385b853c79d4ced2ae7b664a31adefa38
Philip Rydqvist <90709110+PhilipRydqvist@users.noreply.github.com>
2022-11-03 09:34:50 +0100

    password se

---
+++ Backend/Utils/comparePW.js
@@ -0,0 +1,12 @@
+export default function comparePw(pw1, pw2) {
+    if (pw1 !== pw2)
+    {return false} 
+    else 
+    {return true}
+}
+
+export default function passwordLength(pw1, pw2) {
+    if (pw1.length >= 8 && ) {
+        return true
+    } 
+}
