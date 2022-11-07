9da4cd341cc5ac191cd23dd62fe27942c1f92b72
Philip Rydqvist <philip.rydqvist@gmail.com>
2022-11-07 20:44:09 +0100

    få det att funka

--- Backend/SQL/createMockData.js
+++ Backend/SQL/createMockData.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql');
+const bcrypt = require("bcrypt");
 
 require('dotenv').config();
 
@@ -9,7 +10,7 @@ const DB_DATABASE = process.env.DB_DATABASE;
 const DB_PORT = process.env.DB_PORT;
 
 // establish connection to database
-const db = mysql.createConnection({
+const pool = mysql.createConnection({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
@@ -22,7 +23,7 @@ const db = mysql.createConnection({
 //Hårdkoda lösenord 
 const pwdHashed = bcrypt.hashSync("password", 10)
 
-db.connect(async (err, connection) => {
+pool.connect(async (err, connection) => {
   console.log('RUNNING CREATE MOCKDATA SCRIPT');
 let userRoles = `
 INSERT INTO Roles (roleId, rolename) VALUES (1000, "User");
@@ -31,7 +32,7 @@ INSERT INTO Roles (roleId, rolename) VALUES (3000, "BIGBOSS");
 `;
 
 //detta är som att klicka på blixten i mySQL, skapa våra roller 
-db.query(userRoles, async (err) => {
+pool.query(userRoles, async (err) => {
     if (err) {
       console.log(err)
       process.exit(1);
@@ -39,9 +40,16 @@ db.query(userRoles, async (err) => {
     console.log('ROLES CREATED!');
 })
 
-let users = `INSERT INTO Users (userId, username, password) VALUES (null, username, password);`
+/* let users = `INSERT INTO Users (userId, username, password) VALUES (null, username, password);` */
 
-let user = `
+let users = `
+INSERT INTO Users (userId, username, password) VALUES (null, "User", "${pwdHashed}");
+INSERT INTO Users (userId, username, password) VALUES (null, "Admin", "${pwdHashed}");
+INSERT INTO Users (userId, username, password) VALUES (null, "BigBoss", "${pwdHashed}");
+
+`
+
+/* let user = `
 INSERT INTO Users (userId, username, password) VALUES (null, "User", "${pwdHashed}");
 ` 
 let userAdmin = `
@@ -49,29 +57,36 @@ INSERT INTO Users (userId, username, password) VALUES (null, "Admin", "${pwdHash
 `
 let userBigBoss = `
 INSERT INTO Users (userId, username, password) VALUES (null, "BigBoss", "${pwdHashed}");
-`
+` */
+pool.query(users, async (err) => {
+    if (err) {
+      console.log(err)
+      process.exit(1);
+    }
+    console.log('Users CREATED!');
+})
 
-db.query(user, async (err) => {
+/* pool.query(user, async (err) => {
     if (err) {
       console.log(err)
       process.exit(1);
     }
     console.log('User CREATED!');
 })
-db.query(userAdmin, async (err) => {
+pool.query(userAdmin, async (err) => {
     if (err) {
       console.log(err)
       process.exit(1);
     }
     console.log('Admin CREATED!');
 })
-db.query(userBigBoss, async (err) => {
+pool.query(userBigBoss, async (err) => {
     if (err) {
       console.log(err)
       process.exit(1);
     }
-    console.log('BigBoss CREATED!');
-})
+    console.log('BigBoss CREATED!'); */
+
 let usersWithRolesMock = `
 INSERT INTO UsersWithRoles (UserId, RoleId) VALUES (1, 1000);
 INSERT INTO UsersWithRoles (UserId, RoleId) VALUES (2, 1000);
@@ -79,4 +94,13 @@ INSERT INTO UsersWithRoles (UserId, RoleId) VALUES (2, 2000);
 INSERT INTO UsersWithRoles (UserId, RoleId) VALUES (3, 1000);
 INSERT INTO UsersWithRoles (UserId, RoleId) VALUES (3, 2000);
 INSERT INTO UsersWithRoles (UserId, RoleId) VALUES (3, 3000);`
+
+pool.query(usersWithRolesMock, async (err) => {
+    if (err) {
+      console.log(err)
+      process.exit(1);
+    }
+    console.log('UsersWithRolesMock CREATED!');
+})
+
 })
