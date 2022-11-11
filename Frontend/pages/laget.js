e1f1902c494b2bcc9d0ffe3aa38c1eec84390f56
Philip Rydqvist <philip.rydqvist@gmail.com>
2022-11-11 14:09:18 +0100

    Ett försök till att få in bilder via url

--- Frontend/pages/laget.js
+++ Frontend/pages/laget.js
@@ -1,15 +1,25 @@
-import Navbar from '../components/Navbar/Navbar';
-import Footer from '../components/Footer/Footer';
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+import Image from "next/image";
 
 const laget = () => {
-    return (
-        
+  return (
+    <>
+      <div>
+        <p>Info</p>
         <div>
-            
-            <p>Info</p>
-            
-            </div>
-    )
-}
+            {/* ta understa länken på imgbox i Html */}
+          <Image
+            src='https://images2.imgbox.com/76/6d/wspuEbRd_o.jpg'
+            alt='/'
+            width='192'
+            height='288'
+            priority
+          />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default laget;
