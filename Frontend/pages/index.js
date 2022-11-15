62601c2c4f8e49044d2e2da9d2e5509ae634afbd
Philip Rydqvist <philip.rydqvist@gmail.com>
2022-11-15 17:19:10 +0100

    bilder till startsidan.

--- Frontend/pages/index.js
+++ Frontend/pages/index.js
@@ -1,25 +1,32 @@
-import styles from '../styles/Home.module.css'
-import { useRouter } from 'next/router';
-import { useState } from 'react';
-import { useRef } from 'react';
-import Navbar from '../components/Navbar/Navbar';
-import Footer from '../components/Footer/Footer';
-import Axios from 'axios';
+import styles from "../styles/Home.module.css";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { useRef } from "react";
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+import Axios from "axios";
 
-
+/*
+https://images2.imgbox.com/e1/cb/maYrjQ3c_o.png tekning
+https://images2.imgbox.com/e3/1a/F5RIaHRM_o.jpg action 
+https://images2.imgbox.com/1d/b3/ojyjOczN_o.png tackar matchen
+https://images2.imgbox.com/e3/8e/LlAQsP02_o.jpg glada men trötta 
+ */
 export default function Home() {
- 
-
-
   return (
     <div>
-    
-      <main className={styles.main}>
-        <div>Klicka på modal för att registrera och logga</div>
-        
-      </main>
+      <div>
 
-     
+      </div>
+
+      <main className={styles.main}>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+      </main>
     </div>
-  )
+  );
 }
