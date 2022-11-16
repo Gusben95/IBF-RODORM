b1294e6e06ab6f3f843451f9c5780ef10d2979f9
Philip Rydqvist <90709110+PhilipRydqvist@users.noreply.github.com>
2022-11-16 13:10:34 +0100

    css slideshow, inte fått det att funka

--- Frontend/pages/index.js
+++ Frontend/pages/index.js
@@ -1,9 +1,8 @@
- import styles from "../styles/Home.module.css"; 
+import styles from "../styles/Home.module.css";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import { useRef } from "react";
-import Navbar from "../components/Navbar/Navbar";
-import Footer from "../components/Footer/Footer";
+
 import Axios from "axios";
 
 /*
@@ -12,6 +11,7 @@ https://images2.imgbox.com/e3/1a/F5RIaHRM_o.jpg action
 https://images2.imgbox.com/1d/b3/ojyjOczN_o.png tackar matchen
 https://images2.imgbox.com/e3/8e/LlAQsP02_o.jpg glada men trötta 
  */
+
 export default function Home() {
   return (
     <div>
@@ -20,12 +20,68 @@ export default function Home() {
           <h1>VÄLKOMMEN TILL GÖTEBORGS BÄSTA LAG</h1>
           <p className={styles.pEtt}>
             IBF Rödeorm startades 2019 av ett gäng som tidigare spelat innebandy
-            samt andra som följt med bara för att ha kul. Vi har alltid behov av nya
-            spelare så tveka inte att höra av dig för att provträna!
+            samt andra som följt med bara för att ha kul. Vi har alltid behov av
+            nya spelare så tveka inte att höra av dig för att provträna!
+          </p>
+          <p className={styles.pEtt}>
+            Vi spelar i division 1 Göteborgs Korpen och det spelas i
+            lundbystrandshallen.
           </p>
         </div>
-      <div className={styles.divTvåBakgrund}>
-        <div className={styles.divTvåImg}>
+        <div className={styles.divTvåBakgrund}>
+          <div className={styles.slideshowContainer}>
+            <div className={styles.divTvåImg}>
+              <img
+                src="https://images2.imgbox.com/e1/cb/maYrjQ3c_o.png"
+                alt="Tekning"
+              />
+              <p className={styles.textRuta}>Image 1</p>
+            </div>
+            <div className={styles.divTvåImg}>
+              <img
+                loading="lazy"
+                src="https://images2.imgbox.com/e3/1a/F5RIaHRM_o.jpg"
+                alt="Action"
+              />
+              <p className={styles.textRuta}>Image 2</p>
+            </div>
+            <div className={styles.divTvåImg}>
+              <img
+                loading="lazy"
+                src="https://images2.imgbox.com/1d/b3/ojyjOczN_o.png"
+                alt="Tackar alltid domarna och spelare för en bra match!"
+              />
+              <p className={styles.textRuta}>Image 3</p>
+            </div>
+            <div className={styles.divTvåImg}>
+              <img
+                loading="lazy"
+                src="https://images2.imgbox.com/e3/8e/LlAQsP02_o.jpg"
+                alt="Glada vinnare!"
+              />
+              <p className={styles.textRuta}>Image 4</p>
+            </div>
+            <a className={styles.prev} onclick="plusSlides(-1)">
+              &#10094;
+            </a>
+            <a className={styles.next} onclick="plusSlides(1)">
+              &#10095;
+            </a>
+          </div>
+        </div>
+        <div className={styles.dotsDiv}>
+          <span className={styles.dot} onclick="currentSlide(1)"></span>
+          <span className={styles.dotDiv} onclick="currentSlide(2)"></span>
+          <span className={styles.dotDiv} onclick="currentSlide(3)"></span>
+        </div>
+        <div className={styles.divTre}></div>
+      </main>
+    </div>
+  );
+}
+
+{
+  /* <div className={styles.divTvåImg}>
           <img
             className={styles.tekning}
             loading="lazy"
@@ -46,12 +102,5 @@ export default function Home() {
             loading="lazy"
             src="https://images2.imgbox.com/e3/8e/LlAQsP02_o.jpg"
           ></img>
-        </div>
-      </div>
-        <div className={styles.divTre}>
-
-        </div>
-      </main>
-    </div>
-  );
+        </div> */
 }
