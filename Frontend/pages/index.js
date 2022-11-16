ba83e19d20167dc921cbbb4c694a264fa8ed410a
Philip Rydqvist <philip.rydqvist@gmail.com>
2022-11-16 19:48:04 +0100

    halvt fungerande scroll

--- Frontend/pages/index.js
+++ Frontend/pages/index.js
@@ -13,14 +13,13 @@ https://images2.imgbox.com/e3/8e/LlAQsP02_o.jpg glada men trötta
  */
 
 export default function Home() {
-
-//en div för bilderna, sen overflow-scroll with 100%, ligger i diven, kan bara slida 
+  //en div för bilderna, sen overflow-scroll with 100%, ligger i diven, kan bara slida
 
   return (
     <div>
       <main>
         <div className={styles.divEtt}>
-          <h1>VÄLKOMMEN TILL GÖTEBORGS BÄSTA LAG</h1>
+          <h1>VÄLKOMMEN TILL GÖTEBORGS BÄSTA VIKINGALAG</h1>
           <p className={styles.pEtt}>
             IBF Rödeorm startades 2019 av ett gäng som tidigare spelat innebandy
             samt andra som följt med bara för att ha kul. Vi har alltid behov av
@@ -32,53 +31,34 @@ export default function Home() {
           </p>
         </div>
         <div className={styles.divTvåBakgrund}>
-          <div className={styles.slideshowContainer}>
-            <div className={styles.divTvåImg .fade}>
-              <img
-                src="https://images2.imgbox.com/e1/cb/maYrjQ3c_o.png"
-                alt="Tekning"
-              />
-              <p className={styles.textRuta}>Image 1</p>
-            </div>
-            <div className={styles.divTvåImg}>
-              <img
-                loading="lazy"
-                src="https://images2.imgbox.com/e3/1a/F5RIaHRM_o.jpg"
-                alt="Action"
-              />
-              <p className={styles.textRuta}>Image 2</p>
-            </div>
-            <div className={styles.divTvåImg}>
-              <img
-                loading="lazy"
-                src="https://images2.imgbox.com/1d/b3/ojyjOczN_o.png"
-                alt="Tackar alltid domarna och spelare för en bra match!"
-              />
-              <p className={styles.textRuta}>Image 3</p>
-            </div>
-            <div className={styles.divTvåImg}>
-              <img
-                loading="lazy"
-                src="https://images2.imgbox.com/e3/8e/LlAQsP02_o.jpg"
-                alt="Glada vinnare!"
-              />
-              <p className={styles.textRuta}>Image 4</p>
-            </div>
-            <a className={styles.prev} onclick="plusSlides(-1)">
-              &#10094;
-            </a>
-            <a className={styles.next} onclick="plusSlides(1)">
-              &#10095;
-            </a>
+          <div className={styles.divTvåImg}>
+            <img className={styles.tekning}
+              src="https://images2.imgbox.com/e1/cb/maYrjQ3c_o.png"
+              alt="Tekning"
+            />
+
+            <img className={styles.tekning}
+              loading="lazy"
+              src="https://images2.imgbox.com/e3/1a/F5RIaHRM_o.jpg"
+              alt="Action"
+            />
+
+            <img className={styles.tekning}
+              loading="lazy"
+              src="https://images2.imgbox.com/1d/b3/ojyjOczN_o.png"
+              alt="Tackar alltid domarna och spelare för en bra match!"
+            />
+
+            <img className={styles.tekning}
+              loading="lazy"
+              src="https://images2.imgbox.com/e3/8e/LlAQsP02_o.jpg"
+              alt="Glada vinnare!"
+            />
           </div>
         </div>
-        <div className={styles.dotsDiv}>
-          <span className={styles.dot} onclick="currentSlide(1)"></span>
-          <span className={styles.dot} onclick="currentSlide(2)"></span>
-          <span className={styles.dot} onclick="currentSlide(3)"></span>
-          <span className={styles.dot} onclick="currentSlide(4))"></span>
+        <div className={styles.divTre}>
+          <p>Några goa matchbilder från vikingarnas fälttåg</p>
         </div>
-        <div className={styles.divTre}></div>
       </main>
     </div>
   );
