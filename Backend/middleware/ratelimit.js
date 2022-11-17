035ea1dbf9a6ba7ddbf4bbe3f739d01f9b273b3a
Philip Rydqvist <90709110+PhilipRydqvist@users.noreply.github.com>
2022-11-17 13:31:49 +0100

    fixat ratelimiter

--- Backend/middleware/ratelimit.js
+++ Backend/middleware/ratelimit.js
@@ -1,10 +1,10 @@
-import rateLimit from 'express-rate-limit'
+const rateLimit = require("express-rate-limit"); 
 
-const apiLimiter = rateLimit({
+const pingLimiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	max: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
 
-module.exports = { apiLimiter }
+module.exports = { pingLimiter };
--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,6 @@
 const logger = require("./logger/winston");
 const mysql = require("mysql");
-const rateLimit = require ('express-rate-limit')
+
 const bodyParser = require("body-parser");
 const cookie = require("cookie-parser");
 const jwt = require("jsonwebtoken");
@@ -12,9 +12,9 @@ const {
   checkTokenAdminBoss,
   checkTokenBoss,
 } = require("./middleware/jwt");
-/* const { apiLimiter } = require ("./middleware/ratelimit");
-/* const rateLimit = require("express-rate-limit"); 
-import rateLimit from ("express-rate-limit"); */
+ const { pingLimiter } = require ("./middleware/ratelimit");
+const rateLimit = require("express-rate-limit"); 
+
 
 const db = require("./database");
 const { comparePassword, hashPassword } = require("./Utils/bcrypt");
@@ -54,30 +54,30 @@ app.get("/", (req, res) => {
 //skapa användare,asdasdasdda
 //innerhåller: ej skapa likadana konton. Assign roll till användare.
 
-app.post("/createUser", async (req, res) => {
+app.post("/createUser", pingLimiter, async (req, res) => {
   let username = req.body.username;
   let password = req.body.password;
   let hashPassword = await bcrypt.hash(password, 10);
 
   if (!username || !password) {
     res
-      .status(500)
+      .status(400)
       .json({ message: "Username or password missing in request" });
     return;
   }
 
   const userExists = await db.getUserByUsername(username).catch((err) => {
     res
-      .status(500)
+      .status(400)
       .json({ message: "Error getting user to check if already exists " });
   });
   console.log("server.js", userExists);
   if (userExists.length > 0) {
-    res.status(500).json({ message: "User already exists" });
+    res.status(400).json({ message: "User already exists" });
     return;
   }
   const resultId = await db.createUser(username, hashPassword).catch((err) => {
-    res.status(500);
+    res.status(400);
   });
   console.log(resultId);
   db.assignRoleToUser(resultId, 1000);
@@ -90,7 +90,7 @@ app.post("/createUser", async (req, res) => {
 const addMinutes = (minutes, date = new Date()) => {
   return new Date(date.setMinutes(date.getMinutes() + minutes));
 };
-app.post("/loginUser" , async (req, res) => {
+app.post("/loginUser", pingLimiter, async (req, res) => {
   let account = req.body;
   let result = await db.getUserByUsername(account.username).catch((err) => {
     res.send("error");
@@ -126,37 +126,29 @@ app.post("/loginUser" , async (req, res) => {
   }
 });
 
-
-
-app.get("/isLoggedIn", checkTokenAll, async (req, res) => {
+app.get("/isLoggedIn", pingLimiter, checkTokenAll, async (req, res) => {
   let token = req.cookies.token;
-  console.log("loggar toke", token)
+  console.log("loggar toke", token);
   const wholeuser = await db.getUserByToken(token).catch((err) => {
     console.log("Could not get token", err);
     return res.status(400).json({
       message: "ojdå",
-      success: false
+      success: false,
     });
   });
   let user = wholeuser[0];
   user.password = "";
   user.role = req.role;
-  console.log("walla",user);
+  console.log("walla", user);
   res.status(200).json(user);
   /* logger.error(err); */
 });
 
-app.post("/loggOut", async (req, res) => {
-  return res 
-    .clearCookie("token")
-    .status(200)
-    .json({ message: "Logged out" });
-    
-  
-  });
+app.post("/loggOut", pingLimiter, async (req, res) => {
+  return res.clearCookie("token").status(200).json({ message: "Logged out" });
+});
 
-
-app.get("/players", async (req, res) => {
+app.get("/players", pingLimiter, async (req, res) => {
   const playerInfo = await db.getAllPlayers().catch((err) => {
     res.status(400).send("error");
     logger.error(err);
@@ -165,34 +157,22 @@ app.get("/players", async (req, res) => {
   res.status(200).json(playerInfo);
 });
 
-
-app.get("/getusers", checkTokenBoss ,async (req, res) => {
+app.get("/getusers", checkTokenBoss, async (req, res) => {
   const userInfo = await db.getAllUsers().catch((err) => {
     res.status(400).send("error");
     logger.error(err);
     res.end();
-  })
+  });
   res.status(200).json(userInfo);
-})
-
-app.post("/removeuser", checkTokenBoss, async (req, res) => {
-  let user = req.body.userId;
-  console.log("removeuser id = ", user)
-  const deluser = await db.deleteUser(user).catch((err) => { 
-    res.send("error")
 });
-})
 
-
-
-
-
-
-
-
-
-
-
+app.post("/removeuser", pingLimiter, checkTokenBoss, async (req, res) => {
+  let user = req.body.userId;
+  console.log("removeuser id = ", user);
+  const deluser = await db.deleteUser(user).catch((err) => {
+    res.send("error");
+  });
+});
 
 /* const getUserByUsername = (account) => {
   let sql = `SELECT password FROM Users WHERE username=?`;
