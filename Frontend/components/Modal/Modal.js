965fd403b3ba1889b352ffc88718b5c5b4397573
Philip Rydqvist <philip.rydqvist@gmail.com>
2022-11-17 17:58:17 +0100

    nu funkar svagt lösenord denied

--- Frontend/components/Modal/Modal.js
+++ Frontend/components/Modal/Modal.js
@@ -1,9 +1,7 @@
 import styles from "./Modal.module.css";
 import { useState, useRef } from "react";
 import axios from "axios";
-import {useRouter} from "next/router";
-
-
+import { useRouter } from "next/router";
 
 const Modal = () => {
   const usernameRef = useRef(null);
@@ -12,40 +10,43 @@ const Modal = () => {
   const strengthBadge = useRef("");
   const infoh1 = useRef();
   const isEqual = useRef();
-  let strongPassword = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})')
-  let mediumPassword = new RegExp('((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,}))|((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))')
+  let strongPassword = new RegExp(
+    "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})"
+  );
+  let mediumPassword = new RegExp(
+    "((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,}))|((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))"
+  );
+  let weakPassword = (!mediumPassword || !strongPassword);
+
   const [showModal, setShowModal] = useState(false);
-  const [showReg, setReg] = useState(false); 
+  const [showReg, setReg] = useState(false);
   const router = useRouter();
 
   const loginUser = async () => {
-   try {
-    const response = await fetch("http://localhost:4000/loginUser", {
-      
-      method: "POST",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        username: usernameRef.current.value,
-        password: passwordRef.current.value,
-      }),
-    });
-    console.log("rad 36 modal")
-    if (response.status === 200){
-      setShowModal(false)
-      router.push('/profil');
-      return
+    try {
+      const response = await fetch("http://localhost:4000/loginUser", {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: usernameRef.current.value,
+          password: passwordRef.current.value,
+        }),
+      });
+      console.log("rad 36 modal");
+      if (response.status === 200) {
+        setShowModal(false);
+        router.push("/profil");
+        return;
+      }
+      const data = await response.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
     }
-    const data = await response.json();
-    console.log(data);
-   
-   }catch(err){
-    console.log(err);
-   }  
   };
-
 
   const loggOut = () => {
     fetch("http://localhost:4000/loggOut", {
@@ -55,48 +56,50 @@ const Modal = () => {
         "Content-Type": "application/json",
       },
     });
-    router.push('/')
+    router.push("/");
   };
 
   const registerUser = async () => {
-     if(passwordRef.current.value !== passwordrepRef.current.value){
-      isEqual.current.textContent = 'Lösenordet matchar inte'
-      return}
-     if (!strongPassword){
-      isEqual.current.textContent = 'svagt lösenord'
-      return}
-     const response = await fetch("http://localhost:4000/createUser", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: JSON.stringify({
-        username: usernameRef.current.value,
-        password: passwordRef.current.value,
-      }),
-
-    });
-   if (response.status == 200)
-   {setReg(false)}
-  }
-  
-  function strengthChecker(PasswordParameter) {
-    
-   if(!showReg){return}
-    if(strongPassword.test(PasswordParameter)) {
-        strengthBadge.current.style.color = "green";
-        strengthBadge.current.textContent = 'StronK!';
-        
-    } else if(mediumPassword.test(PasswordParameter)) {
-        strengthBadge.current.style.color = 'blue';
-        strengthBadge.current.textContent = 'Medium';
+    if (passwordRef.current.value !== passwordrepRef.current.value) {
+      isEqual.current.textContent = "Lösenordet matchar inte";
+      return;
+    } if (passwordRef.current.value !== strongPassword) {
+      isEqual.current.textContent = "svagt lösenord";
+      return;
     } else {
-        strengthBadge.current.style.color = 'red';
-        strengthBadge.current.textContent = 'Weak';
+      const response = await fetch("http://localhost:4000/createUser", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify({
+          username: usernameRef.current.value,
+          password: passwordRef.current.value,
+        }),
+      });
+      if (response.status == 200) {
+        setReg(false);
+      }
     }
-}
+  };
+
+  function strengthChecker(PasswordParameter) {
+    if (!showReg) {
+      return;
+    }
+    if (strongPassword.test(PasswordParameter)) {
+      strengthBadge.current.style.color = "green";
+      strengthBadge.current.textContent = "StronK!";
+    } else if (mediumPassword.test(PasswordParameter)) {
+      strengthBadge.current.style.color = "blue";
+      strengthBadge.current.textContent = "Medium";
+    } else {
+      strengthBadge.current.style.color = "red";
+      strengthBadge.current.textContent = "Weak";
+    }
+  }
 
   return (
     <div>
@@ -113,8 +116,11 @@ const Modal = () => {
               e.stopPropagation();
             }}
           >
-            <h1 ref = {infoh1}className={styles.textLoggaIn}> { showReg ? 'register': 'Logga in' }</h1>
-           
+            <h1 ref={infoh1} className={styles.textLoggaIn}>
+              {" "}
+              {showReg ? "register" : "Logga in"}
+            </h1>
+
             <p
               className={styles.exit}
               onClick={() => {
@@ -127,38 +133,38 @@ const Modal = () => {
             <input
               type="password"
               placeholder="Password"
-              ref={passwordRef} onChange= {(e) => {
-                strengthChecker(passwordRef.current.value)
+              ref={passwordRef}
+              onChange={(e) => {
+                strengthChecker(passwordRef.current.value);
               }}
             ></input>
             {showReg ? (
-            <>
-            <input
-              type="password"
-              placeholder="Password"
-              ref={passwordrepRef} onChange= {(e) => {
-                strengthChecker(passwordrepRef.current.value)
-               
-                }}
-            ></input>
-            <p ref = {strengthBadge}> Lösenords styrka </p>
-            <p ref = {isEqual}></p>
-            </>
+              <>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  ref={passwordrepRef}
+                  onChange={(e) => {
+                    strengthChecker(passwordrepRef.current.value);
+                  }}
+                ></input>
+                <p ref={strengthBadge}> Lösenords styrka </p>
+                <p ref={isEqual}></p>
+              </>
             ) : (
-            <button
-              className={styles.loginBtn}
-              type="submit"
-              onClick={loginUser}
-            >
-              Logga in
-            </button>
-            ) }
+              <button
+                className={styles.loginBtn}
+                type="submit"
+                onClick={loginUser}
+              >
+                Logga in
+              </button>
+            )}
             <div>
               <button
                 className={styles.registerBtn}
                 type="submit"
-                onClick={ showReg ? registerUser : setReg
-                }
+                onClick={showReg ? registerUser : setReg}
               >
                 Registrera
               </button>
@@ -167,8 +173,14 @@ const Modal = () => {
         </div>
       )}
 
-     <button onClick={() => {setShowModal(true)}}>Logga in</button>
-      <button onClick ={loggOut} >Logga ut</button>
+      <button
+        onClick={() => {
+          setShowModal(true);
+        }}
+      >
+        Logga in
+      </button>
+      <button onClick={loggOut}>Logga ut</button>
     </div>
   );
 };
